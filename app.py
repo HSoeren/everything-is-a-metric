f38a9bb0aeee5b68ef19164a58f1b71ae6674461
Soeren Helms <soeren.helms1@gmail.com>
2023-05-22 12:38:36 +0200

    dev script, index file, flask basics

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import json
+from functions import load_strichliste, save_strichliste, generate_metrics
+
 app = Flask(__name__)
 strichliste = {}
 strichliste = load_strichliste()
@@ -18,3 +20,13 @@ def increment_count(name):
     requests_counter.inc()
 
     return 'OK', 200
+
+@app.route('/')
+def info():
+    return render_template('info.html')
+
+def main():
+    app.run(debug=False, host='0.0.0.0', port=3400)
+
+if __name__ == '__main__':
+    main()
